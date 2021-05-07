9290fa766c3e6f44c1ba08f94b4453f0447f84ba
Kent <75441565+kburchill@users.noreply.github.com>
2021-05-06 21:55:19 -0600

    Fixed form visibiltiy

--- react-app/src/components/forms/application-form.js
+++ react-app/src/components/forms/application-form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import './form.css'
 
@@ -21,6 +21,8 @@ const CreateApplicationForm = () => {
   const [interview_date, setInterview_date] = useState();
   const [interview_contact, setInterview_contact] = useState("");
   const [interview_type, setInterview_type] = useState("");
+  const [showResponse, setShowResponse] = useState(false);
+  const [showInterview, setShowInterview] = useState(false);
 
   const userId = user.id;
 
@@ -36,70 +38,73 @@ const CreateApplicationForm = () => {
     interview_type: interview_type
   }
 
+  useEffect(() => {
+    setShowResponse(response)
+    setShowInterview(interview);
+  }, [response, interview])
 
   //We can add update in here and use this form
 
   return (
-      <div className="form-body">
-        <label>Where did you apply?</label>
-        <select onChange={(e) => setCompany_id(e.target.value)}>
-          <option selected value="1">Apple</option>
-          <option value="2">Facebook</option>
-          <option value="3">Amazon</option>
-        </select>
-        <label>When did you send your application?</label>
-        <input
-          name="date"
-          type="date"
-          className="form-input"
-          value={sent_out}
-          onChange={(e) => setSent_date(e.target.value)}
-        />
-        <label>Have you heard back?</label>
-        <select onChange={(e) => setResponse(e.target.value)}>
-          <option value="true">Yes</option>
-          <option selected value="false">No</option>
-        </select>
-        <label hidden={!response}>Awesome! When did you hear back?</label>
+    <div className="form-body">
+      <label>Where did you apply?</label>
+      <select onChange={(e) => setCompany_id(e.target.value)}>
+        <option selected value="1">Apple</option>
+        <option value="2">Facebook</option>
+        <option value="3">Amazon</option>
+      </select>
+      <label>When did you send your application?</label>
+      <input
+        name="date"
+        type="date"
+        className="form-input"
+        value={sent_out}
+        onChange={(e) => setSent_date(e.target.value)}
+      />
+      <label>Have you heard back?</label>
+      <select onChange={(e) => setResponse(e.target.value)}>
+        <option value="true">Yes</option>
+        <option selected value="false">No</option>
+      </select>
+        <label className={`view-${showResponse}`}>Awesome! When did you hear back?</label>
         <input
           hidden={!response}
           name="date"
           type="date"
-          className="form-input"
+          className={`form-input view-${showResponse}`}
           value={response_date}
           onChange={(e) => setResponse_date(e.target.value)}
         />
-        <label hidden={!response}>Do you have an interview date?</label>
-        <select hidden={!response} onChange={(e) => setInterview(e.target.value)}>
-          <option value="true">Yes</option>
-          <option selected value="false">No</option>
-        </select>
-        <label hidden={!interview}>Woohoo! When is your interview?</label>
-        <input hidden={!interview}
-          name="date"
-          type="date"
-          className="form-input"
-          value={interview_date}
-          onChange={(e) => setInterview_date(e.target.value)}
-        />
-        <label hidden={!interview}>Who are you in contact with for your interview?
-                </label>
-        <textarea
-          hidden={!interview}
-          name="contact-name"
-          value={interview_contact}
-          onChange={(e) => setInterview_contact(e.target.value)}
-          className="form-input"
-        />
-        <label hidden={!interview}>Who type of interview is this?</label>
-        <textarea
-          hidden={!interview}
-          name="contact-name"
-          value={interview_type}
-          onChange={(e) => setInterview_type(e.target.value)}
-          className="form-input"
-        />
-      </div>
+      <label className={`view-${showResponse}`}>Do you have an interview date?</label>
+      <select className={`view-${showResponse}`} onChange={(e) => setInterview(e.target.value)}>
+        <option value="true">Yes</option>
+        <option selected value="false">No</option>
+      </select>
+      <label className={`view-${showInterview}`}>Woohoo! When is your interview?</label>
+      <input
+        name="date"
+        type="date"
+        className={`form-input view-${showInterview}`}
+        value={interview_date}
+        onChange={(e) => setInterview_date(e.target.value)}
+      />
+      <label className={`view-${showInterview}`} hidden={!interview}>Who are you in contact with for your interview?
+      </label>
+      <input
+        name="contact-name"
+        value={interview_contact}
+        onChange={(e) => setInterview_contact(e.target.value)}
+        className={`form-input view-${showInterview}`}
+      />
+      <label className={`view-${showInterview}`} hidden={!interview}>Who type of interview is this?</label>
+      <input
+        hidden={!interview}
+        name="contact-name"
+        value={interview_type}
+        onChange={(e) => setInterview_type(e.target.value)}
+        className={`form-input view-${showInterview}`}
+      />
+    </div>
 
   );
 };
